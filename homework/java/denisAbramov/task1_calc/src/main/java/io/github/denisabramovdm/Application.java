4d7590d0538abbb671906626c5c66094196e627d
DenisAbramovDm <sdgnhfs@gmail.com>
2021-02-14 12:01:46 +0300

    added tests

--- homework/java/denisAbramov/task1_calc/src/main/java/io/github/denisabramovdm/Application.java
+++ homework/java/denisAbramov/task1_calc/src/main/java/io/github/denisabramovdm/Application.java
@@ -1,15 +1,16 @@
 package io.github.denisabramovdm;
 
+import io.github.denisabramovdm.bean.Inputs;
 import io.github.denisabramovdm.service.Calc;
 
 public class Application {
 
     public static void main(String[] args) {
+        Inputs inputs = new Inputs();
+        Calc calc = new Calc(inputs);
 
-        Calc calc = new Calc();
-
-        calc.inputing();
-        calc.calculating();
+        calc.inputing(inputs);
+        calc.calculating(inputs);
 
 
     }
--- homework/java/denisAbramov/task1_calc/src/main/java/io/github/denisabramovdm/service/Calc.java
+++ homework/java/denisAbramov/task1_calc/src/main/java/io/github/denisabramovdm/service/Calc.java
@@ -4,8 +4,16 @@ import io.github.denisabramovdm.bean.Inputs;
 import java.util.Scanner;
 
 public class Calc {
-    Inputs inputs = new Inputs();
-public void calculating(){
+
+
+    //  Inputs inputs = new Inputs();
+
+    public Calc(Inputs inputs) {
+
+    }
+
+
+    public void calculating(@org.jetbrains.annotations.NotNull Inputs inputs){
     switch (inputs.getOperation()) {
         case "+": {inputs.setResult(inputs.getNumber1() + inputs.getNumber2());
         break;}
@@ -19,15 +27,9 @@ public void calculating(){
         break;}
     }
     System.out.println(inputs.getResult());
-
-
 }
 
-
-
-    public void inputing(){
-
-
+    public void inputing(@org.jetbrains.annotations.NotNull Inputs inputs){
         System.out.println("Введите первое число");
         Scanner in = new Scanner(System.in);
         inputs.setNumber1(in.nextDouble());
@@ -37,8 +39,6 @@ public void calculating(){
 
         System.out.println("Введите второе число");
         inputs.setNumber2(in.nextDouble());
-
-
         in.close();
 
     }
