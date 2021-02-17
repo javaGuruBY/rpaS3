78117658be133067e3a51c169cba47aa8bbd7ec7
bumax1989 <bumax1989@gmail.com>
2021-02-17 17:08:56 +0300

    addds checkUserPaswordFunctionality

--- myFeature/loginFeaure/src/main/java/by/jrr/service/LoginService.java
+++ myFeature/loginFeaure/src/main/java/by/jrr/service/LoginService.java
@@ -1,4 +1,9 @@
 package by.jrr.service;
 
+import by.jrr.bean.User;
+
 public class LoginService {
+    public boolean checkUserPassword(User user, String userInput) {
+        return user.getPassword().equals(userInput);
+    }
 }
--- myFeature/loginFeaure/src/test/java/by/jrr/service/LoginServiceTest.java
+++ myFeature/loginFeaure/src/test/java/by/jrr/service/LoginServiceTest.java
@@ -1,7 +1,48 @@
 package by.jrr.service;
 
-import static org.junit.Assert.*;
+import by.jrr.bean.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class LoginServiceTest {
+    LoginService loginService;
+
+    @Before
+    public void setUp(){
+        this.loginService=new LoginService();
+    }
+
+    @Test
+    public void checkUserPassword_positive(){
+        User user = getUser();
+
+        String userInput = "password";
+
+        boolean actualResult = loginService.checkUserPassword(user, userInput);
+
+        Assert.assertTrue(actualResult);
+
+    }
+
+
+
+    @Test
+    public void checkUserPassword_negative(){
+        User user = getUser();
+
+        String userInput = "wrongPassword";
+
+        boolean actualResult = loginService.checkUserPassword(user, userInput);
+
+        Assert.assertFalse(actualResult);
+
+    }
+
+    private User getUser() {
+        User user = new User();
+        user.setPassword("password");
+        return user;
+    }
 
 }
