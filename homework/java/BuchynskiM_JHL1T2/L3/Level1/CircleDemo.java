577e29e0da4bf821da49187f26de0cb1c4754b50
Maksim Buchynski <61361054+bumax1989@users.noreply.github.com>
2021-02-21 12:07:04 +0300

    Create class CircleDemo.java

---
+++ homework/java/BuchynskiM_JHL1T2/L3/Level1/CircleDemo.java
@@ -0,0 +1,10 @@
+package Lecture3_Level1;
+
+public class CircleDemo {
+    public static void main(String[] args) {
+      
+        Circle circle = new Circle(5);
+
+        System.out.println("Area of the circle = "+circle.calculateArea());
+    }
+}
