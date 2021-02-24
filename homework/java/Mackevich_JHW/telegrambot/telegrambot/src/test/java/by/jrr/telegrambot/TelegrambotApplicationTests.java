0c6c13277b6df1674d6d3f0aeaafc8f83d3eea4b
AlexMackevich <78922190+AlexMackevich@users.noreply.github.com>
2021-02-24 10:18:21 +0200

    Add files via upload

---
+++ homework/java/Mackevich_JHW/telegrambot/telegrambot/src/test/java/by/jrr/telegrambot/TelegrambotApplicationTests.java
@@ -0,0 +1,13 @@
+package by.jrr.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegrambotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
