523fc20db7e0825bbe7b2e547f171eeed32c3dc0
vklda <78853752+vklda@users.noreply.github.com>
2021-02-26 16:55:52 +0300

    adds checkUserPasswordTest functionality

--- kolyadaFeatureLogin/src/main/java/io/github/vkdla/service/LoginService.java
+++ kolyadaFeatureLogin/src/main/java/io/github/vkdla/service/LoginService.java
@@ -1,5 +1,10 @@
 package io.github.vkdla.service;
 
+import io.github.vkdla.bean.User;
+
 public class LoginService {
 
+    public boolean checkUserPassword(User user, String userInput) {
+        return user.getPassword().equals(userInput);
+    }
 }
--- kolyadaFeatureLogin/src/test/java/io/github/vkdla/service/LoginServiceTest.java
+++ kolyadaFeatureLogin/src/test/java/io/github/vkdla/service/LoginServiceTest.java
@@ -1,7 +1,40 @@
 package io.github.vkdla.service;
 
-import static org.junit.Assert.*;
+import io.github.vkdla.bean.User;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 
 public class LoginServiceTest {
+    private LoginService loginService;
+    @Before
+    public void setUp(){
+        this.loginService = new LoginService();
+    }
+    @Test
+    public void checkUserPassword_positive() {
+        User user = getUser();
 
+        String userInput = "admin";
+
+        boolean actualResult = loginService.checkUserPassword(user, userInput);
+        Assert.assertTrue(actualResult);
+    }
+
+    @Test
+    public void checkUserPassword_negative() {
+        User user = getUser();
+
+        String userInput = "password";
+
+        boolean actualResult = loginService.checkUserPassword(user, userInput);
+        Assert.assertFalse(actualResult);
+    }
+
+    private User getUser() {
+        User user = new User();
+        user.setPassword("admin");
+        return user;
+    }
 }
